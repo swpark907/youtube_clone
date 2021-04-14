0e51a8466b89a19d8f3b24609a4a21f52607157b
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-04-14 11:50:54 +0900

    make video item

--- src/components/video-item/video-item.jsx
+++ src/components/video-item/video-item.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
+const VideoItem = ({video: {snippet}}) => (
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <img src={snippet.thumbnails.medium.url}  className={styles.thumbnail}></img>
+            <div className={styles.desc}>
+                <p className={styles.title}>{snippet.title}</p>
+                <p className={styles.channel}>{snippet.channelTitle}</p>
+            </div>
+        </div>
 
-    <h1>
-        
-        {props.video.snippet.title}
-    </h1>
-    
+    </li>
     );
 
 export default VideoItem;
--- src/components/video-list/video_list.jsx
+++ src/components/video-list/video_list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import VideoItem from '../video-item/video-item';
+import styles from '../video-item/video_item.module.css';
 
 const VideoList = (props) => (
-        <ul>
+        <ul className={styles.videos}>
             {props.videos.map(video => 
                 <VideoItem key={video.id}  video={video}/>
             )}
